d1d32e0885d831995c5cb726b78788ad38274ded
michael-korir <michaelkiptoo2000@gmail.com>
2023-10-02 18:21:26 +0300

    Arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 31;
+let num2 =2;
+let multiply= (num1*num2);
+console.log(multiply);
+
+//Generates a random integer greater than 0
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+// Is an equation that calculates a remainder that is equal to 4
+const num3 = 24; 
+const num4 = 5;  
+const mod = num3 % num4;
+
+//Will return 20 as the highest number in the set
+const numbersSet = [10, 15, 20, 5];
+const max = Math.max(...numbersSet);
